fad8fc70b3127deaca3f0ca3fc76ea8018901ccd
Shreya Deexit <shreyadeexit@Shreyas-MacBook-Pro.local>
2023-09-13 15:29:42 -0400

    added assignment features to main branch

---
+++ pages/index.tsx
@@ -0,0 +1,78 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '@/styles/Home.module.css'
+import mainImage from '@/assets/images/ai_image.jpg'
+import { Form, Button, Spinner } from 'react-bootstrap'
+import { FormEvent, useState } from 'react'
+
+export default function Home() {
+
+  const [quote, setQuote] = useState("");
+  const [quoteLoading, setQuoteLoading] = useState(false);
+  const [quoteLoadingError, setQuoteLoadingError] = useState(false);
+
+  async function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const formData = new FormData(e.target as HTMLFormElement);
+    const prompt = formData.get("prompt")?.toString().trim();
+
+    if (prompt) {
+      try {
+        setQuote("");
+        setQuoteLoadingError(false);
+        setQuoteLoading(true);
+
+        const response = await fetch("/api/cringe?prompt=" + encodeURIComponent(prompt));
+        const body = await response.json();
+        setQuote(body.quote);
+      } catch (error) {
+        console.error(error);
+        setQuoteLoadingError(true);
+      } finally {
+        setQuoteLoading(false);
+      }
+    }
+  }
+
+  return (
+    <>
+      <Head>
+        <title>Ask AI - Get answers to your questions</title>
+        <meta name="description" content="by Coding in Flow" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={styles.main}>
+        <h1>Ask Anything</h1>
+        <h2>powered by GPT-3.5</h2>
+        <div>Enter a topic and the AI will answer your questions</div>
+        <div className={styles.mainImageContainer}>
+          <Image
+            src={mainImage}
+            width={300}
+      height={300}
+            alt='A picture'
+            priority
+            className={styles.mainImage}
+          />
+        </div>
+        <Form onSubmit={handleSubmit} className={styles.inputForm}>
+          <Form.Group className='mb-3' controlId='prompt-input'>
+            <Form.Label>Type your question below...</Form.Label>
+            <Form.Control
+              name='prompt'
+              placeholder='e.g. How was the earth created?'
+              maxLength={100}
+            />
+          </Form.Group>
+          <Button type='submit' className='mb-3' disabled={quoteLoading}>
+            Get Answer
+          </Button>
+        </Form>
+        {quoteLoading && <Spinner animation='border' />}
+        {quoteLoadingError && "Something went wrong. Please try again."}
+        {quote && <h5>{quote}</h5>}
+      </main>
+    </>
+  )
+}
